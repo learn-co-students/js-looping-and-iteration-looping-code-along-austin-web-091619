b6990ccb800ebb484fe2f8e090dd1b6018508ec2
Joellehelm <joellenhelm@gmail.com>
2019-10-27 15:31:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards(name, type) {
+    let arr = []
+    for (let i = 0; i < name.length; i++) {
+        arr.push(`Thank you, ${name[i]}, for the wonderful ${type} gift!`)
+    }
+    return arr
+}
+
+function countDown(num) {
+    for ( let i = num; i > -1; i--) {
+        console.log(i)
+    }
+}
