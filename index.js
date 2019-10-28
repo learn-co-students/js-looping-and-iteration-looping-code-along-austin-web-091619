7df736dff1268b123acbd6932a0d1d5c7a42d7a3
swdecker <swdecker@gmail.com>
2019-10-28 14:18:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrNames, strEvent) {
+    const cards = [];
+    for(let i=0; i < arrNames.length; i++){
+        cards.push(`Thank you, ${arrNames[i]}, for the wonderful ${strEvent} gift!`);
+    }
+    return cards;
+}
+function countDown(startNum){
+    let i = startNum;
+    while( i >= 0){
+        console.log(i);
+        i--;
+    }
+}
